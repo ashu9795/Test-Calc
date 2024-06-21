d39b535a10efdc572a5fa920f64104fd0b69df75
Ashutosh9795 <ashutoshg067@gmail.com>
2024-06-21 11:01:07 +0530

    this is for setupping the environment

--- test_calc/src/index.js
+++ test_calc/src/index.js
@@ -1,6 +1,7 @@
+// src/index.js
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import App from './App';  // Adjust path as per your project structure
 
 ReactDOM.render(
   <React.StrictMode>
